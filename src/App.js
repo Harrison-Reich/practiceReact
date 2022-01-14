11e3be4488472ee06319b0c06db25409383b0894
Harrison Reich <hreich1998@gmail.com>
2022-01-14 14:28:09 -0800

    code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import Navbar from './components/Navbar'
+
+const title = `Harrison Reich's Favorites!`
+
+const book = {
+  title: 'The richest man in Babylon',
+  author: 'Samuel Clason',
+  published: 1926
+}
+
+const song = {
+  title: 'Money Trees',
+  artist: 'Kendrick Lamar',
+  released: 2012
+}
+
+const movie = {
+  title: 'Tenet',
+  director: 'Christopher Nolan',
+  rated: 'R'
+}
+
+const videogame = {
+  title: 'League of Legends',
+  publisher: 'Riot Games',
+  published: 2009
+}
+
 const App = () => {
   return (
     <>
-    
+      <Navbar title={title} />
     </>
   )
 }
